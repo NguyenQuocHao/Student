35517b7cdb16376dd0ae41dd6ea84149e663d214
NguyenQuocHao <nghao@sheridancollege.ca>
2019-04-05 22:05:02 -0400

    Save

--- src/ca/sheridancollege/project/CardBlackJack.java
+++ src/ca/sheridancollege/project/CardBlackJack.java
@@ -11,6 +11,25 @@ package ca.sheridancollege.project;
  * @author nguyen
  */
 public class CardBlackJack extends Card {
+    
+    public enum Suit {HEARTS, CLUBS, SPADES, DIAMONDS};
+    public enum Value {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+    private Suit suit;
+    private Value value;
+
+    public CardBlackJack(Suit s, Value gVal) {
+        suit = s;
+        value = gVal;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
     @Override
     public String toString(){
         return "";
--- src/ca/sheridancollege/project/DealerBlackJack.java
+++ src/ca/sheridancollege/project/DealerBlackJack.java
@@ -1,6 +1,7 @@
 package ca.sheridancollege.project;
 
 public class DealerBlackJack extends PlayerBlackJack {
+    
     public void setBet(double bet){
         
     }
